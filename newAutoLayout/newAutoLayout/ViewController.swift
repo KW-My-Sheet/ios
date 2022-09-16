778f6d837f0e5f2dfddd4e2ef0f1f224ed96c14f
jusong23 <jusong23@naver.com>
2022-09-16 20:04:45 +0900

    NSLayout

--- newAutoLayout/newAutoLayout/ViewController.swift
+++ newAutoLayout/newAutoLayout/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 클로저로 뷰를 설정
+    var myThirdView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.layer.cornerRadius = 5
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true // corner radius 전용
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.myFirstView()
+    }
+    
+    func myFirstView() {
+        
+        // MARK: - First View
         let myFirstView = UIView()
         myFirstView.translatesAutoresizingMaskIntoConstraints = false
         // 필수 !
@@ -23,13 +39,28 @@ class ViewController: UIViewController {
         myFirstView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true // x축 위치
         myFirstView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200).isActive = true // y축 위치
         
-        myFirstView.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        myFirstView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        myFirstView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        myFirstView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         myFirstView.layer.cornerRadius = 30
+        
+        let mySecondView = UIView()
+        mySecondView.backgroundColor = .green
+        mySecondView.layer.cornerRadius = 10
+        mySecondView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // MARK: - Second View
+        self.view.addSubview(mySecondView)
+        NSLayoutConstraint.activate([
+            mySecondView.leadingAnchor.constraint(equalTo: myFirstView.leadingAnchor, constant: 10),
+            mySecondView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            mySecondView.widthAnchor.constraint(equalToConstant: 60),
+            mySecondView.heightAnchor.constraint(equalToConstant: 60)
+        ])
+        // .isActive = true 줄이기
+        
     }
-
-
+    
 }
 
 // Swift UI 프리뷰 제공 메소드
@@ -40,12 +71,12 @@ class ViewController: UIViewController {
 import SwiftUI
 struct ViewControllerRepresentable: UIViewControllerRepresentable {
     
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
+    func updateUIViewController(_ uiView: UIViewController,context: Context) {
         // leave this empty
-}
-@available(iOS 13.0.0, *)
-func makeUIViewController(context: Context) -> UIViewController{
-    ViewController()
+    }
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        ViewController()
     }
 }
 @available(iOS 13.0, *)
